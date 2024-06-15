82da6b77e09bab031ebe64822430e76fcfc912e4
simpleFran <frank.m.camargo@gmail.com>
2024-06-15 15:30:58 -0300

    last updated

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 ################
 
-# [x] - Change the chicken icon (A bit confuse)
-# [x] - A better icon positioning when a specific zoom is applied (zoom between 3 and 8).  
-# [x] - There are a CORS error when papaParse get downloading the csv file. Try to host the csv file in my own github repository or something...
-# [] - Download the complete github repository and install (npm install)
-# [] - 
+# [0] - Change the chicken icon (A bit confuse)
+# [0] - A better icon positioning when a specific zoom is applied (zoom between 3 and 8).  
+# [0] - There are a CORS error when papaParse get downloading the csv file. Try to host the csv file in my own github repository or something...
+# [0] - Download the complete github repository and install (npm install)
+
+# [] - When upstream github, give the credits to Coding With Adam
+
+
+
+
+
+
+----------------------------------------------------------------------
+# [] - Check how to give a name to each git version. Isn't a priority yet.
--- src/tasks/LoadCountriesTask.js
+++ src/tasks/LoadCountriesTask.js
@@ -1,33 +1,65 @@
 import Papa from "papaparse";
-import features from "../data/file.json";
-import Oscar from '../cases_country.csv';
+// import {features as F} from "../data/file.json";
+import { features } from "../data/file.json";
+import _archive from "../cases_country.csv";
 
 class LoadCountryTask {
-  covid19DataUrl =
-    "http://github.com/simpleFran/interactiveMap/blob/master/cases_country.txt";
   setState = null;
-
+  // mapCountries = features;
+  covidUrl =
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv";
   load = (setState) => {
     this.setState = setState;
-    Papa.parse(Oscar, {
+
+    Papa.parse(this.covidUrl, {
       // delimiter: "",
       download: true,
       header: true,
-      
-      complete: (results) => {
-        this.#processCovidData(results.data);
-        console.log("RESULTADO:",results);
-        
-      },
-      
+
+      complete: (result) => this.#processCovidData(result.data),
+      // console.log(result);
     });
-    this.setState = setState;
-    setState(features);
+    // this.setState = setState;
+    // setState(features);
   };
 
   #processCovidData = (covidCountries) => {
-    console.log(covidCountries);
+    // console.log(countryObj.length)
+    for (let i = 0; i < features.length; i++) {
+      const country = features[i];
+      console.log(country);
+      const covidCountry = covidCountries.find(
+        (covidCountry) => country.properties.ISO_A3 === covidCountry.ISO3
+      );
+
+      country.properties.confirmed = 0;
+      country.properties.confirmedText = 0;
+
+      // if (covidCountry != null) {
+      //   let confirmed = Number(covidCountry.Confirmed);
+      //   country.properties.confirmed = confirmed;
+      //   country.properties.confirmedText = this.#formatNumberWithCommas(
+      //     confirmed
+      //   );
+      // }
+      // this.#setCountryColor(country);
+    }
+
+    this.setState(features);
   };
+  // #processCovidData = (covidCountries) => {
+  //   for (let i = 0; i < features.length; i++) {
+  //     const country = features[i];
+  //     console.log('test:',country)
+  //     const _covidCountry = covidCountries.find(
+  //       (covidCountry) => covidCountry.ISO3 === country.properties.ISO_A3
+  //     );
+  //     //initialize this new property 'cofirmed'
+  //     country.properties.confirmed = 0;
+  //     country.properties.confirmedText = "0";
+  //   }
+  //   this.setState(features);
+  // };
 }
 
 export default LoadCountryTask;
